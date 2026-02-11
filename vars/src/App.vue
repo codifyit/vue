bca45f4e3dd8537088e24c1490594d7a019fe0a4
codifyit <rmenmar264@g.educaand.es>
2026-02-11 12:58:11 +0100

    Main component

--- vars/src/App.vue
+++ vars/src/App.vue
@@ -1,12 +1,14 @@
-<script setup lang="ts"></script>
+<script setup>
+  let number = 0;
+</script>
 
 <template>
   <h1>You did it!</h1>
   <p>
     Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
     documentation
-    
   </p>
+  <p>{{number}}</p>
 </template>
 
 <style scoped></style>
